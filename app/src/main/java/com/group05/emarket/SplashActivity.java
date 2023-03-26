e70587893931e1fd3160be78763f547e0c6eb99b
Tâm Lê <leduytam600@gmail.com>
2023-03-26 17:09:41 +0700

    ME-04 | Create splash screen UI & change icon launcher

---
+++ app/src/main/java/com/group05/emarket/SplashActivity.java
@@ -0,0 +1,19 @@
+package com.group05.emarket;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            new Handler().postDelayed(() -> {
+                startActivity(new Intent(SplashActivity.this, HomeActivity.class));
+                finish();
+            }, 1500);
+        }
+}
