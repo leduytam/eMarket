8356a2518d7c79f9ed119f3d8ea14f8b4134c74c
Bun <hoangquocnam021117@gmail.com>
2023-03-30 16:28:40 +0700

    ME-05 | Update color theme

--- app/src/main/java/com/group05/emarket/AuthenticationActivity.java
+++ app/src/main/java/com/group05/emarket/AuthenticationActivity.java
@@ -1,14 +1,49 @@
 package com.group05.emarket;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class AuthenticationActivity extends AppCompatActivity {
+
+    private Button mSignupButton;
+    private Button mLoginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_authentication);
+//        requestWindowFeature(getWindow().FEATURE_NO_TITLE);
+
+        // Get references to views in the layout
+        mSignupButton = findViewById(R.id.signup_button);
+        mLoginButton = findViewById(R.id.login_button);
+
+        // Set click listener for sign up button
+        mSignupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO: Implement sign up functionality
+
+                // For now, just start the SignupActivity
+//                Intent intent = new Intent(AuthenticationActivity.this, SignupActivity.class);
+//                startActivity(intent);
+            }
+        });
+
+        // Set click listener for login button
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO: Implement login functionality
+
+                // For now, just start the LoginActivity
+//                Intent intent = new Intent(AuthenticationActivity.this, LoginActivity.class);
+//                startActivity(intent);
+            }
+        });
     }
 }
