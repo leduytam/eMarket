eae92f3499989c09cf9aa502feb776285172fb12
Bun <hoangquocnam021117@gmail.com>
2023-04-08 03:16:22 +0700

    ME-15 | Implement carousel banner for app

---
+++ app/src/main/java/com/group05/emarket/models/BannerItem.java
@@ -0,0 +1,13 @@
+package com.group05.emarket.models;
+
+public class BannerItem {
+    private int mImageResId;
+
+    public BannerItem(int imageResId) {
+        mImageResId = imageResId;
+    }
+
+    public int getImageResId() {
+        return mImageResId;
+    }
+}
